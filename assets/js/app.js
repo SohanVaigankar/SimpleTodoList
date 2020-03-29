afe51475cecf8be82817164c10681528a1f6aa11
Sohan Vaigankar <52046276+SohanVaigankar@users.noreply.github.com>
2020-03-29 17:30:40 +0530

    Initial Commit

---
+++ assets/js/app.js
@@ -0,0 +1,46 @@
+// Marking to as completed
+
+/*Method-1
+$("li").on("click", function() {
+  if ($(this).css("color") === "rgb(128, 128, 128)") {
+    $(this).css({ color: "black", textDecoration: "none" });
+  } else {
+    $(this).css("color", "gray");
+    $(this).css("text-decoration", "line-through");
+  }
+});*/
+//OR
+
+/*Here, the event listener '.on("click")' will trigger for all the new lists '.list' that are added to 'ul' .list-container  */
+$(".list-container").on("click", ".text", function(event) {
+  $(this).toggleClass("completed"); //class .completed adds strike-through feature to the list
+  event.stopPropagation();
+});
+
+//Deleting Todo
+$(".list-container").on("click", ".delete", function(event) {
+  $(this)
+    .parent()
+    .fadeOut(500, function() {
+      $(this).remove();
+    }); //removes parent element (li) with fading out effect
+  event.stopPropagation();
+});
+
+//Adding new Todo
+$(".add-todo").keypress(function(event) {
+  if (event.which === 13) {
+    var newTodo = $(this).val();
+    $(this).val("");
+    $(".list-container").append(
+      '<li class = "list"><span class = "delete"><img src="./assets/icons/trash-can.svg" alt="trash-can" /> </span><div class = "text">' +
+        newTodo +
+        "</div></li> "
+    ); //append() will append new todo list to the existing ul container
+  }
+});
+
+//linking add button to jquery
+$(".add-todo-logo").on("click", function() {
+  $(".add-todo").fadeToggle();
+});
